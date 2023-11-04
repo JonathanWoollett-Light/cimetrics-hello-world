a1f3ffb44858e0dd01412f806a1b79318c161d6e
Jonathan <jonathanwoollettlight@gmail.com>
2023-11-04 22:57:17 +0000

    e
    a

--- diff.sh
+++ diff.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-DATA=$( cat ./metrics )
-echo "data: { $DATA }"
-
 curl \
     -X GET \
-    -d "{ \"user\": { \"public_key\": \"6546b543a35b7d5af8c93a7b\", \"private_key\": $PRIVATE_KEY }, \"from\": \"$FROM\", \"to\": $TO }" \
+    -d "{ \"user\": { \"public_key\": \"6546b543a35b7d5af8c93a7b\", \"private_key\": $PRIVATE_KEY }, \"from\": \"$FROM\", \"to\": \"$TO\" }" \
     -H "Content-Type: application/json" \
     -i http://3.10.39.149:3000/diff_pretty
--- upload.sh
+++ upload.sh
@@ -5,6 +5,6 @@ echo "data: { $DATA }"
 
 curl \
     -X POST \
-    -d "{ \"user\": { \"public_key\": \"6546b543a35b7d5af8c93a7b\", \"private_key\": $PRIVATE_KEY }, \"sha\": \"$GITHUB_SHA\", \"metrics\": { $DATA } }" \
+    -d "{ \"user\": { \"public_key\": \"6546b543a35b7d5af8c93a7b\", \"private_key\": $PRIVATE_KEY }, \"sha\": \"$TO\", \"metrics\": { $DATA } }" \
     -H "Content-Type: application/json" \
     -i http://3.10.39.149:3000/metrics
