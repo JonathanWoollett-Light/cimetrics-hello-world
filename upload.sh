868a4d3c012ab05224ac6db95b58b1711b80e3af
Jonathan <jonathanwoollettlight@gmail.com>
2023-11-04 22:40:49 +0000

    init

---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DATA=$( cat ./metrics )
+echo "data: { $DATA }"
+
+curl \
+    -X POST \
+    -d "{ \"user\": { \"public_key\": \"6546b543a35b7d5af8c93a7b\", \"private_key\": $PRIVATE_KEY }, \"sha\": \"$GITHUB_SHA\", \"metrics\": { $DATA } }" \
+    -H "Content-Type: application/json" \
+    -i http://3.10.39.149:3000/metrics
