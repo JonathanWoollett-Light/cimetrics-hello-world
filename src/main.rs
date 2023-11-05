ae0e3425aae5847c289cc1d8d019b86f11ce9120
Jonathan <jonathanwoollettlight@gmail.com>
2023-11-05 21:21:37 +0000

    a

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
     println!("I'm going for a walk");
-    println!("aaaaaaaaaaaaa");
+    println!("aaaaaaaaaaaaaa");
     println!("Goodbye, world!");
 }
